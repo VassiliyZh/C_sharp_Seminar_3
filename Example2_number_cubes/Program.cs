e93b4e3a30c142eb0e0f44cb988cf328892ffae6
VassiliyZh <111jvv@mail.ru>
2023-01-05 00:52:15 +0500

    Выводим линейку чисел, возведенных в куб

---
+++ Example2_number_cubes/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вводим число n и на выходе получаем числовой ряд по возразтанию до n, 
+// каждый из которых возведенный в куб
+string TableCubes(int n) 
+{
+    string answer = string.Empty;
+
+    for (int i = 1; i<=n; i++)
+    {
+        answer+=(i==n)? Math.Pow(i,3).ToString() : Math.Pow(i,3).ToString() + ",";
+    }
+    return answer;
+}
+int n = 6;
+Console.WriteLine(TableCubes(n));
