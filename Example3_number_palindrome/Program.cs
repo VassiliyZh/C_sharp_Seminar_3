06c5dbc299f836536ac24d709361bc52798c6eae
VassiliyZh <111jvv@mail.ru>
2023-01-05 17:00:57 +0500

    определяем является ли введенное число палиндромо

---
+++ Example3_number_palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вводим любое число и проверяем является ли оно палиндромом.
+
+public static class Extensions
+{
+    public static string Reverse(this string str)
+    {
+        char[] chars = str.ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+}
+ 
+public class Example
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число");
+        string str = Console.ReadLine();
+ 
+        string reverse = str.Reverse();
+        if (str == reverse)
+        {
+            Console.WriteLine("Это число является Полиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Набрали неверное число");
+        }
+    }
+}
